4f78303e4f282aef2b1fa8c23b7be37a11bfe412
ayly8 <allison.y.ly@gmail.com>
2024-05-05 16:15:31 -0700

    added comments

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from dotenv import load_dotenv, find_dotenv
 from flask import Flask, request, jsonify, render_template
 from openai import OpenAI
 
+# create Flask server
 app = Flask(__name__)
+# hide API key
 load_dotenv(find_dotenv())
 api_key = os.environ.get("API_KEY")
 
@@ -18,26 +20,30 @@ def render_homepage():
 # create api endpoint called generate_plan to handle POST requests
 @app.route('/generate_plan', methods=['POST'])
 def generate_plan():
+   # receives the user input
    data = request.get_json()
    x = data['x']
    y = data['y']
    z = data['z']
-    
+   # puts the xyz into the prompt
    prompt = f"Plan {x} for {y} about {z}"
+   # call the chatgpt api to get the suggested result
    response = client.chat.completions.create(
                model="gpt-3.5-turbo",
                messages=[{"role": "user", 
                         "content": prompt}],
                temperature=0.6,
-               max_tokens=120,
+               max_tokens=4096,
                top_p=1,
                frequency_penalty=0,
                presence_penalty=0
             )
+   # store the result into variable suggested_plan
    suggested_plan = response.choices[0].message.content
    
    # returns suggested plan in form of js dictionary (key: value) format
    return jsonify({'suggestion': suggested_plan})
 
+# run Flask server
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,10 @@
 function display_sug() {
+    //get user input and store into variables
     var x = document.getElementById("x").value;
     var y = document.getElementById("y").value;
     var z = document.getElementById("z").value;
 
+    // send POST request to /generate_plan endpoint with the data from input fields
     fetch('/generate_plan', {
         method: 'POST',
         headers: {
@@ -12,6 +14,7 @@ function display_sug() {
     })
     .then(response => response.json())
     .then(data => {
+        // updates website with suggested result from chatgpt api
         document.getElementById("suggestion_result").innerText = data.suggestion;
     });
 }
